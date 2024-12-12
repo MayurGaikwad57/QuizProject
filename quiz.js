b671094b043a0e61d3283ba5fe196bc83c74fef7
Mayur <mayur.gaikwad@avaali.com>
2024-12-12 09:57:25 +0530

    [Timer Added Into Application]

--- quiz.js
+++ quiz.js
@@ -1,14 +1,20 @@
+const container = document.querySelector('.container')
 const questions = document.querySelector('.quizQuestions');
 const options = document.querySelector('.quizOptions');
 const answers = document.querySelector('.quizAnswers');
 const getScore = document.querySelector('.scoreCard')
 const getAlert = document.querySelector('.alert');
-const nextBtn = document.querySelector('.btn');
+const nextBtn = document.querySelector('.nextBtn');
+const startBtn = document.querySelector('.startBtn')
+const timerbtn = document.querySelector('.timer');
+
 
 
 let questionIndex = 0;
 let score = 0;
 let quizOver = false;
+let timeLeft = 15;
+let timeStop = null;
 
 const quiz = [
     {
@@ -44,7 +50,7 @@ const quiz = [
 ]
 
 let showQuestions = ()=> {
-    // getAlert.style.display = "none";
+    timerbtn.style.display = "block";
   let questionDetails = quiz[questionIndex];
   questions.textContent = questionDetails.question;
    options.textContent = "";
@@ -64,6 +70,9 @@ let showQuestions = ()=> {
         }
       })
   }
+  if(questionIndex<quiz.length){
+    showTimer();
+  }
   
 } 
 
@@ -76,8 +85,9 @@ const showAnswer = () => {
     }
     else{
         // alert("Wrong Answer")
-        showAlert("Wrong Answer");
+        showAlert(`Wrong Answer !! The Correct Answer is ${quiz[questionIndex].answer}`);
     }
+    timeLeft = 15;
     questionIndex++;
     if(questionIndex < quiz.length) {
         showQuestions();
@@ -85,56 +95,85 @@ const showAnswer = () => {
     else{
         showScore();
         quizOver = true;
+        stopTimer();
+        timerbtn.style.display = "none";
     }
 }
 
 const showAlert = (msg) => {
   getAlert.textContent = msg;
   getAlert.style.display = "block";
+  setTimeout(()=>{
+ getAlert.style.display ="none";
+  },2000)
+//   getAlert.style.display = "block";
 }
 
+const showTimer = () => {
+    clearInterval(timeStop);
+    timerbtn.textContent = timeLeft;
+    // timeLeft--;
+    let countDown = () => {
+        timeLeft--;
+        timerbtn.textContent = timeLeft;
+        if(timeLeft === 0) {
+            const confirmUser = confirm("Time's Up!! Do you want to play Again")
+            if(confirmUser) {
+                timeLeft = 15;
+                startQuiz();
+            }
+            else {
+                startBtn.style.display = "block";
+                container.style.display = "none";
+                return;
+            }
+        }
+    }
+   timeStop = setInterval(countDown,1000)
+}
+let startQuiz = () => {
+    timeLeft = 15;
+    timerbtn.style.display = "flex";
+    showQuestions();
+}
+let stopTimer = () => {
+    clearInterval(timeStop);
+}
 const showScore = () => {
     getAlert.style.display = "none"
     questions.textContent = "";
     options.textContent ="";
     getScore.textContent = `You scored ${score} out of ${quiz.length}`
+    showAlert("Quiz Completed")
     nextBtn.textContent = "Play Again";
-    
-    
+    // timerbtn.style.display = "none"
 }
-showQuestions();
+// showQuestions();
 nextBtn.addEventListener('click',()=>{
    const selectedOption = document.querySelector('.choice.selected');
    if(!selectedOption && nextBtn.textContent === "Next") {
     //    alert("please select an answer");
     showAlert("please select an answer");
     getAlert.style.display = "block";
+    return;
    }
    else if(quizOver) {
      nextBtn.textContent = "Next";
      getScore.textContent ="";
      questionIndex = 0;
-     showQuestions();
-     quizOver = false;
      score =0;
-     getAlert.style.display = "block";
+     quizOver = false;
+     startQuiz();
+    //  getAlert.style.display = "none";
    }
    else {
     showAnswer();  
    }
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+startBtn.addEventListener('click',()=>{
+    startBtn.style.display = 'none';
+    container.style.display = 'block';
+    startQuiz();
+})
 
